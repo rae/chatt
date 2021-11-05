1b6bd9804cf83bef027110dd3a739094076ddd52
Reid Ellis <rae@tnir.org>
2021-11-05 10:12:00 -0400

    Initial commit

---
+++ Shared/ChattApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattApp.swift
+//  Shared
+//
+//  Created by Reid Ellis on 2021-11-05.
+//
+
+import SwiftUI
+
+@main
+struct ChattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
